4a3a7225cedb0dbf947d5e9bc4697abb5e88aac2
wilmar mac <wilmar.sepulveda@correounivalle.edu.co>
2025-09-30 20:53:16 -0500

    Finalizando clase 1

---
+++ src/basics/diccionarios.py
@@ -0,0 +1,8 @@
+edades_amigos = {
+    "wilmar": 30,
+    "jennyfer": 28,
+    "herney": 35
+}
+print(edades_amigos)
+print("La edad de Wilmar es: ")
+print(edades_amigos["wilmar"])
---
+++ src/basics/listas.py
@@ -0,0 +1,15 @@
+nombres = ["Wilmar", #0
+           "Andrea", #1
+           "Herney"] #2
+
+print("Estos son mis amigos: ",  nombres)
+
+num10_1 = [1,2,3,4,5,6,7,8,9,10]
+#breakpoint()
+print(num10_1)
+num10_2 = list(range(1,11))
+print(num10_2)
+
+
+num10_3 = [num for num in range(1,11)]
+print(num10_3)
